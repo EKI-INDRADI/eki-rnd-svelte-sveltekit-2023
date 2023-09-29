da869e46b12b2064a7979a79a7ceacb78e34d687
EKI-INDRADI <eki.indradi@gmail.com>
2023-09-29 20:59:56 +0700

    EKI-20230929-014-If-template

--- demo/src/App.svelte
+++ demo/src/App.svelte
@@ -1,44 +1,20 @@
 <script>
-  let name = "Joe";
-  // let capitalizedName = name.toUpperCase()  //  changes bind issue
-  $: capitalizedName = name.toUpperCase(); // changes bind work
-  let age = 33;
-  $: dogYears = age * 7; // statement support changes bind
-
-  let red = 10;
-  let blue = 0;
-  let green = 0;
-let highGreen = false
-  $: colorValue = color(red, green, blue);
-  $: console.log(green)
-  $: {
-    console.log(green, "green")
-    console.log(red, "red")
-    console.log(blue, "blue")
-  }
-
-  $: if(green > 100) {
-    highGreen =  true
-  }
-
-  function color(req, green, blue) {
-    return `rgb{${red},${green},${blue}}`;
-  }
-
-  setTimeout(() => {
-    green += 150;
-
-  }, 1500);
+  let colorName = "blue";
+  let isChecked =  false
 </script>
 
-<h2>{name}</h2>
-<h2>{capitalizedName}</h2>
-<input type="text" bind:value={name} />
+{#if colorName.toLowerCase() === "blue"}
+  <h2>Blue</h2>
+{:else if colorName.toLowerCase() === "red"}
+  <h2>Red</h2>
+{:else}
+  <h2>Some other color</h2>
+{/if}
 
-<h2>Age : {age}</h2>
-<h2>Dog years : {dogYears}</h2>
-<input type="number" bind:value={age} />
+<input type="text" bind:value={colorName}>
 
-<h2>Color Value : {colorValue}</h2>
-<!-- <h2>Color Value : {color(red,green,blue)}</h2> -->
-<h2>{highGreen ? 'HIGH' : 'LOW'} Green</h2>
+<input type="checkbox" bind:checked={isChecked}>
+<hr />
+{#if isChecked}
+<h2>You Win!!!</h2>
+{/if}
