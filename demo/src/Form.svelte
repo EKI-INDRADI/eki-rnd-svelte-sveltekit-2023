75b19e6d4a849176c7e5b68f9dad163fb732a088
EKI-INDRADI <eki.indradi@gmail.com>
2023-09-29 13:24:49 +0700

    EKI-20230929-011-Custom-Events

---
+++ demo/src/Form.svelte
@@ -0,0 +1,26 @@
+<script>
+    let name = "blue"   
+    let age = 33
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher()
+    function onSubmit(e) {
+        dispatch('form_submit',{
+            // name : name,
+            // age : age
+            name,
+            age
+        })
+    }
+</script>
+
+
+<form on:submit|preventDefault={onSubmit}>
+    <label for="name">Name</label>
+    <input type="text" bind:value={name} id="">
+    <label for="age">Age</label>
+    <input type="number" bind:value={age} id="">
+    <button type="submit">Submit</button>
+</form>
+
+
+<button on:click>Click Me</button>
