811d7b8940895d2a0e593d5af59a997484eb0c4b
EKI-INDRADI <eki.indradi@gmail.com>
2023-10-01 20:40:15 +0700

    EKI-20231001-016-Each-Template-Id

--- demo/src/App.svelte
+++ demo/src/App.svelte
@@ -10,27 +10,34 @@
   let color = "";
 
   function addColor() {
-    // if (color !== "") { // allow duplicate
-    if (color !== "" && !colorList.includes(color)) { // unique
+    // if (color !== '') { // allow duplicate
+    if (color !== "" && !colorList.includes(color)) {
+      // unique
       colorList = [color, ...colorList];
+      color = "";
     } else {
-      alert('Color must not be blank or already be in the list.')
+      alert("Color must not be blank or already be in the list.");
     }
   }
 </script>
 
 <ul>
-
   <!-- {#each colorList as color}  << allow duplicate -->
   <!-- {#each colorList as color (color)} << unique -->
-  
+
+  <!--- Each Template -->
+
   <!-- {#each colorList as color (color)} 
     <li>{color}</li>
   {/each} -->
 
-  {#each colorList as color, index (color)} 
-  <li>{index + 1}) {color}</li>
-{/each}
+  <!--- /Each Template -->
+
+  <!--- Each Template Id -->
+  {#each colorList as color, index (color)}
+    <li>{index + 1}) {color}</li>
+  {/each}
+  <!--- Each Template Id -->
 </ul>
 
 <input type="text" bind:value={color} />
