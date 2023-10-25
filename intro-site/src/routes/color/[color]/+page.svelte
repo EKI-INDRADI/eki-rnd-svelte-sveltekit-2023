cbc06f08cb28bd676700ebf4d680e32825f72f35
EKI-INDRADI <ngxcrypt@gmail.com>
2023-10-25 15:25:34 +0700

    EKI-20231025-034-Routing-Parameters

---
+++ intro-site/src/routes/color/[color]/+page.svelte
@@ -0,0 +1,20 @@
+
+
+<script>
+    /** @type {import('./$types').PageLoad} */
+    export let data;
+</script>
+
+
+<!-- http://localhost:5173/color/blue
+http://localhost:5173/color/red -->
+<div style="background-color: {data.color};">
+
+</div>
+
+<style>
+    div {
+        width: 400px;
+        height: 400px;
+    }
+</style>
