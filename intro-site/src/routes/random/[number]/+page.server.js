1b660d3ad5de78f1b5d6564972cdc5306acc5190
EKI-INDRADI <eki.indradi@gmail.com>
2023-11-10 16:13:51 +0700

    EKI-20231110-035-Server-Load-Function-With-Environment-Variables

---
+++ intro-site/src/routes/random/[number]/+page.server.js
@@ -0,0 +1,10 @@
+
+
+import { RANDOM_NUMBER } from '$env/static/private';
+
+export function load({  params }) {
+    const selectedNumber = +params.number
+    return {
+        won: selectedNumber === +RANDOM_NUMBER
+    }
+}
