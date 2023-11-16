e7a69873d028babd5d494870dccda081d11b5d23
EKI-INDRADI <ngxcrypt@gmail.com>
2023-11-16 15:58:11 +0700

    EKI-20231116-039-Challenge-build-a-personal-webpage

---
+++ personal-site/src/routes/contact/+page.svelte
@@ -0,0 +1,16 @@
+
+
+<!-- div.row>div.col -->
+
+<div class="row">
+    <div class="col">
+        <h1>Contact</h1>
+    </div>
+</div>
+
+
+<div class="row">
+    <div class="col">
+        <p>Contact me at xxxx@gmail.com</p>
+    </div>
+</div>
